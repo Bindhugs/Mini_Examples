50db32b02e2d36c8393b1535feebf4a4204668d4
BindhuGS <b6243451@gmail.com>
2026-03-18 16:06:14 +0530

    Simple_Interest_Calculator.py

---
+++ Simple_Interest_Calculator.py
@@ -0,0 +1,9 @@
+# Simple_Interest_Calculator
+
+p = float(input("Principal: "))
+r = float(input("Rate: "))
+t = float(input("Time: "))
+
+SI = (p * r * t) / 100
+
+print("Simple interest of",p,r,t,"is",SI)
