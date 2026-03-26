8dcf4530ad1ff06172e0e0ff3c1e38302d99197d
BindhuGS <b6243451@gmail.com>
2026-03-26 20:02:19 +0530

    square_number.py

---
+++ Squares_number.py
@@ -0,0 +1,7 @@
+numbers = [1,2,3,4,5,6]
+
+result1 = [x*x for x in numbers if x%2==0] #for even number
+result2 = [x*x for x in numbers if x%2!=0] #for odd number
+
+print("Squares of even numbers: ",result1)
+print("Squares of odd number: ",result2)
