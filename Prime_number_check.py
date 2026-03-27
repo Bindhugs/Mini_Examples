c44fc7f727c14a13e014bdff2996930e46367c3b
BindhuGS <b6243451@gmail.com>
2026-03-27 22:19:15 +0530

    prime_number_checker.py

---
+++ Prime_number_check.py
@@ -0,0 +1,13 @@
+#CHECKS IF A NUMBER IS PRIME OR NOT
+
+n = int(input("Enter a number: "))
+
+if n > 1:
+    for i in range(2, n):
+        if n % i == 0:
+            print("Not Prime")
+            break
+    else:
+        print("Prime")
+else:
+    print("Not Prime")
