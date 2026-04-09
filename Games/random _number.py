9124bfc9b92a34d55582f88c99d84ffc9d6150a1
BindhuGS <b6243451@gmail.com>
2026-04-09 19:26:26 +0530

    random_number.py

--- Games/random _number.py
+++ Games/random _number.py
@@ -2,18 +2,21 @@ import random
 # Number Guessing Game (1–50)
 # The program generates a random number and asks the user to guess it.
 
-while True:
-    target = random.randint(1, 50)
-    while True:
-            user_choice = int(input("Guess the target (1-50): "))
+rand = random.randint(1,100)
 
-            if user_choice == target:
-                print("Success: Correct Answer")
-                break
-            elif user_choice < target:
-                print("Take a bigger guess")
-            else:
-                print("Take a smaller guess")
+while True:
+    try:
+        number = int(input("Guess the number between 1 to 100: "))
+        if (number < rand):
+            print("Too low")
+        elif (number > rand):
+            print("Too high")
+        else:
+            print("Congragulations! You guessed the number 😉")
+    except ValueError:
+        print("Please enter a valid number")
+    
+    
     # Replay option
     play_again = input("\nPlay again? (y/n): ").lower()
     if play_again != "y":
