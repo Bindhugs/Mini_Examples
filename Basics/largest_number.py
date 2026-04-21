0126a7819f924c7eead01d34741e00d6914fd78e
Bindhu <b6424351@gmail.com>
2026-04-21 18:56:32 +0530

    sentence_count.py

--- Basics/largest_number.py
+++ Basics/largest_number.py
@@ -1,3 +1,5 @@
+#This will show the largest of 3 numbers
+
 a = int(input("Enter first number: "))
 b = int(input("Enter second number: "))
 c = int(input("Enter third number: "))
--- String_Programs/count_sentence.py
+++ String_Programs/count_sentence.py
@@ -1,3 +1,5 @@
+#This will count the number of words in a sentence
+
 sentence = input("Enter the sentence in which you want to count the words: ")
 
 words = sentence.split()
