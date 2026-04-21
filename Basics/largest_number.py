8be4e66167c02c6e80b5e9896aad13dda2344666
Bindhu <b6424351@gmail.com>
2026-04-21 18:42:31 +0530

    largest_number.py

---
+++ Basics/largest_number.py
@@ -0,0 +1,10 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+
+if a > b and a > c:
+    print(f'{a} is the largest element among {a},{b},{c}')
+elif b > c and b > a:
+    print(f'{b} is the largest element among {a},{b},{c}')
+else:
+    print(f'{c} is the largest element among {a},{b},{c}')
---
+++ String_Programs/count_sentence.py
@@ -0,0 +1,6 @@
+sentence = input("Enter the sentence in which you want to count the words: ")
+
+words = sentence.split()
+count = len(words)
+
+print("Number of words: ",count)
